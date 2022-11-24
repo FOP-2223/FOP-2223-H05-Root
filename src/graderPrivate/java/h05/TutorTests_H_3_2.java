3d93a45a733d5d48fb3649e63599ae0a3f4e08c2
Daniel Mangold <mangold.daniel@protonmail.com>
2022-11-24 19:51:29 +0100

    Use message utils instead of normal JUnit assertions

--- src/graderPrivate/java/h05/TutorTests_H_3_2.java
+++ src/graderPrivate/java/h05/TutorTests_H_3_2.java
@@ -2,12 +2,14 @@ package h05;
 
 import org.junit.jupiter.api.Test;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+import org.tudalgo.algoutils.reflect.ClassTester;
+import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.function.IntSupplier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.*;
 
 @TestForSubmission
 public class TutorTests_H_3_2 {
@@ -31,18 +33,23 @@ public class TutorTests_H_3_2 {
     }
 
     private void test_chargeable(byte n) throws InvocationTargetException, IllegalAccessException {
-        Field specificType_field =
-            H05_Tester.ELECTRIC_BOAT_CT.get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_SPECIFIC_TYPE_AM.get());
+        Field specificType_field = H05_Tester.ELECTRIC_BOAT_CT
+            .get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_SPECIFIC_TYPE_AM.get());
         Object instance = H05_Tester.ELECTRIC_BOAT_CT.get().resolve().resolveInstance();
-        H05_Tester.ELECTRIC_BOAT_CT.get().setField(instance, specificType_field, n);
+        ClassTester.setField(instance, specificType_field, n);
 
-        boolean standardVoltageChargable =
-            (boolean) H05_Tester.ELECTRICALLY_DRIVEN_STANDARD_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
-        boolean highVoltageChargable =
-            (boolean) H05_Tester.ELECTRICALLY_DRIVEN_HIGH_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
+        boolean standardVoltageChargeable = (boolean) H05_Tester.ELECTRICALLY_DRIVEN_STANDARD_VOLTAGE_CHARGEABLE_MT
+            .get().resolveMethod().invoke(instance);
+        boolean highVoltageChargeable = (boolean) H05_Tester.ELECTRICALLY_DRIVEN_HIGH_VOLTAGE_CHARGEABLE_MT
+            .get().resolveMethod().invoke(instance);
 
-        assertEquals(n == 6 || n == 11 || n == 12 || n == 22, standardVoltageChargable, "standardVoltageChargeable liefert falschen Wert zurück.");
-        assertEquals((n % 2 == 0) & ((n + 1) % 3 == 0), highVoltageChargable, "highVoltageChargeable liefert falschen Wert zurück.");
+        Context context = contextBuilder()
+            .add("specificType", n)
+            .build();
+        assertEquals(n == 6 || n == 11 || n == 12 || n == 22, standardVoltageChargeable, context, result ->
+            "standardVoltageChargeable returns an unexpected value");
+        assertEquals((n % 2 == 0) & ((n + 1) % 3 == 0), highVoltageChargeable, context, result ->
+            "highVoltageChargeable returns an unexpected value");
     }
 
     @Test
@@ -55,22 +62,24 @@ public class TutorTests_H_3_2 {
 
     @Test
     public void test_chargeable_return() {
-        fail("Not implemented: *VolatgeChargeable only return");
+        fail(emptyContext(), result -> "Not implemented: *VoltageChargeable only return");
     }
 
     @Test
     public void test_letsGo() throws InvocationTargetException, IllegalAccessException {
-        Field currentCharge_field = H05_Tester.ELECTRIC_BOAT_CT.get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
-        currentCharge_field.setAccessible(true);
-        Field capacity_field = H05_Tester.ELECTRIC_BOAT_CT.get().assureClassResolved().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CAPACITY_AM.get());
-        capacity_field.setAccessible(true);
+        Field currentCharge_field = H05_Tester.ELECTRIC_BOAT_CT
+            .get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
+        currentCharge_field.trySetAccessible();
+        Field capacity_field = H05_Tester.ELECTRIC_BOAT_CT
+            .get().assureClassResolved().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CAPACITY_AM.get());
+        capacity_field.trySetAccessible();
         Object instance = H05_Tester.ELECTRIC_BOAT_CT.get().assureClassResolved().resolveInstance();
 
         int[] capacity_TV = {1};
         int[] currentCharge_TV = {2};
         byte[] additionalChargeVolume_TV = {3};
 
-        for (int i = 0; i < capacity_TV.length; i++){
+        for (int i = 0; i < capacity_TV.length; i++) {
             //H05_Tester.ELECTRIC_BOAT_CT.get().assureClassResolved().setField(currentCharge_field, currentCharge_TV[i]);
             //H05_Tester.ELECTRIC_BOAT_CT.get().assureClassResolved().setField(capacity_field, capacity_TV[i]);
 
@@ -80,29 +89,42 @@ public class TutorTests_H_3_2 {
             H05_Tester.ELECTRICALLY_DRIVEN_LETS_GO_MT.get().resolveMethod().invoke(instance, additionalChargeVolume_TV[i], 0);
 
             int expected_currentCharge = Math.min(capacity_TV[i], currentCharge_TV[i] + additionalChargeVolume_TV[i]);
-            int actual_currentCharge = (int)currentCharge_field.get(instance);
-            assertEquals(expected_currentCharge, actual_currentCharge, "currentCharge entspricht nicht dem erwarteten Wert!");
+            int actual_currentCharge = (int) currentCharge_field.get(instance);
+
+            Context context = contextBuilder()
+                .add("additionalChargeVolume", additionalChargeVolume_TV[i])
+                .add("distance", 0)
+                .build();
+            assertEquals(expected_currentCharge, actual_currentCharge, context, result ->
+                "currentCharge does not match the expected value");
         }
 
         //TODO check if let me move was called
-        fail("Not implemented: check letMeMove calles");
+        fail(emptyContext(), result -> "Not implemented: check letMeMove calls");
     }
 
 
     private void singleLetMeMoveTest(int currentCharge, int distance) throws IllegalAccessException, InvocationTargetException {
-        Field currentCharge_field = H05_Tester.ELECTRIC_BOAT_CT.get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
-        currentCharge_field.setAccessible(true);
+        Field currentCharge_field = H05_Tester.ELECTRIC_BOAT_CT
+            .get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
+        currentCharge_field.trySetAccessible();
         Object instance = H05_Tester.ELECTRIC_BOAT_CT.get().assureClassResolved().resolveInstance();
 
         currentCharge_field.set(instance, currentCharge);
-        int actual_return = (int)H05_Tester.MEANS_OF_TRANSPORT_LET_ME_MOVE_MT.get().resolveMethod().invoke(instance, distance);
-        int actual_currentCharge = (int)currentCharge_field.get(instance);
+        int actual_return = (int) H05_Tester.MEANS_OF_TRANSPORT_LET_ME_MOVE_MT.get().resolveMethod().invoke(instance, distance);
+        int actual_currentCharge = (int) currentCharge_field.get(instance);
 
         int expected_currentCharge = Math.max(0, currentCharge - Math.min(distance / 100, 1));
         int expected_return = currentCharge - expected_currentCharge;
 
-        assertEquals(expected_currentCharge, actual_currentCharge, "currentCharge ist nicht korrekt berechnet.");
-        assertEquals(expected_return, actual_return, "Der Rückgabewert ist nicht korrekt");
+        Context context = contextBuilder()
+            .add("currentCharge", currentCharge)
+            .add("distance", distance)
+            .build();
+        assertEquals(expected_currentCharge, actual_currentCharge, context, result ->
+            "currentCharge is not calculated correctly");
+        assertEquals(expected_return, actual_return, context, result ->
+            "the return value is incorrect");
     }
 
 
@@ -114,31 +136,40 @@ public class TutorTests_H_3_2 {
     @Test
     public void test_getAsInt() throws InvocationTargetException, IllegalAccessException {
         var electric_boat = H05_Tester.ELECTRIC_BOAT_CT.get().resolve();
-        Field currentCharge_field =
-            electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
-        Field capacity_field =
-            electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CAPACITY_AM.get());
-
+        Field currentCharge_field = electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
+        Field capacity_field = electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CAPACITY_AM.get());
         Object instance = electric_boat.resolveInstance();
 
-        int capacity = (int) electric_boat.setFieldRandom(capacity_field);
         int currentCharge = (int) electric_boat.setFieldRandom(currentCharge_field);
-
+        int capacity = (int) electric_boat.setFieldRandom(capacity_field);
         int returned_value = ((IntSupplier) instance).getAsInt();
-        assertEquals(capacity - currentCharge, returned_value, "getAsInt liefert nicht den korrekten Wert zurück.");
+
+        Context context = contextBuilder()
+            .add("currentCharge", currentCharge)
+            .add("capacity", capacity)
+            .build();
+        assertEquals(capacity - currentCharge, returned_value, context, result ->
+            "getAsInt did not return the expected value");
     }
 
     private void test_setSpecificType(byte specificType) throws InvocationTargetException, IllegalAccessException { // TODO: Same hier, resolve erstellt neue instance
         var electric_boat = H05_Tester.ELECTRIC_BOAT_CT.get().resolve();
-        Field specificType_field =
-            H05_Tester.ELECTRIC_BOAT_CT.get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_SPECIFIC_TYPE_AM.get());
+        Field specificType_field = H05_Tester.ELECTRIC_BOAT_CT
+            .get().resolve().resolveAttribute(H05_Tester.ELECTRIC_BOAT_SPECIFIC_TYPE_AM.get());
         Object instance = electric_boat.resolveInstance();
         byte expected_old_specificType = (byte) electric_boat.setFieldRandom(specificType_field);
-        byte actual_old_specificType = (byte) H05_Tester.ELECTRIC_BOAT_SET_SPECIFIC_TYPE_MT.get().resolveMethod().invoke(instance,
-            specificType);
+        byte actual_old_specificType = (byte) H05_Tester.ELECTRIC_BOAT_SET_SPECIFIC_TYPE_MT
+            .get().resolveMethod().invoke(instance, specificType);
         byte actual_new_specificType = (byte) electric_boat.getFieldValue(specificType_field);
-        assertEquals(Math.max(0, Math.min(30, specificType)), actual_new_specificType, "setSpecificType setzt den Wert des Attributs specificType nicht korrekt");
-        assertEquals(expected_old_specificType, actual_old_specificType, "setSpecificType liefert nicht den alten Wert des Attributs specificType zurück.");
+
+        Context context = contextBuilder()
+            .add("old specificType", expected_old_specificType)
+            .add("specificType parameter", specificType)
+            .build();
+        assertEquals(Math.max(0, Math.min(30, specificType)), actual_new_specificType, context, result ->
+            "setSpecificType did not update the specificType field");
+        assertEquals(expected_old_specificType, actual_old_specificType, context, result ->
+            "setSpecificType did not return the field's old value");
     }
 
     @Test
@@ -151,7 +182,7 @@ public class TutorTests_H_3_2 {
 
     @Test
     public void test_setSpecificType_no_ternary() {
-        fail("Not implemented: setSpecificType does not use ternary");
+        fail(emptyContext(), result -> "Not implemented: setSpecificType does not use ternary operator");
     }
 
 
@@ -160,27 +191,36 @@ public class TutorTests_H_3_2 {
         var means_of_transport = H05_Tester.MEANS_OF_TRANSPORT_CT.get().resolve();
 
         var currentCharge_field = electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CURRENT_CHARGE_AM.get());
-        currentCharge_field.setAccessible(true);
+        currentCharge_field.trySetAccessible();
         var capacity_field = electric_boat.resolveAttribute(H05_Tester.ELECTRIC_BOAT_CAPACITY_AM.get());
-        capacity_field.setAccessible(true);
+        capacity_field.trySetAccessible();
         var transportType_field = means_of_transport.resolveAttribute(H05_Tester.MEANS_OF_TRANSPORT_TRANSPORT_TYPE_AM.get());
-        transportType_field.setAccessible(true);
+        transportType_field.trySetAccessible();
 
         var constructor = electric_boat.resolveConstructor(H05_Tester.ELECTRIC_BOAT_CONSTRUCTOR_PARAMETER_MATCHERS.get());
 
-        Object instance = constructor.newInstance((byte)0, currentCharge, capacity);
+        Object instance = constructor.newInstance((byte) 0, currentCharge, capacity);
 
         int expected_capacity = Math.min(0, capacity);
         int expected_currentCharge = Math.min(expected_capacity, Math.max(0, currentCharge));
         Object expected_transportType = null;
 
-        int actual_capacity = (int)capacity_field.get(instance);
-        int actual_currentCharge = (int)currentCharge_field.get(instance);
+        int actual_capacity = (int) capacity_field.get(instance);
+        int actual_currentCharge = (int) currentCharge_field.get(instance);
         var actual_transportType = transportType_field.get(instance);
 
-        assertEquals(expected_capacity, actual_capacity, "Attribut Capacity wird nicht korrekt gesetzt.");
-        assertEquals(expected_currentCharge, actual_currentCharge, "Attribut currentCharge wird nicht korrekt gesetzt.");
-        assertEquals("VESSEL", actual_transportType.toString(), "Attribut transportType wird nicht korrekt gesetzt."); //TODO: Nicht so clean
+        Context context = contextBuilder()
+            .add("specificType", 0)
+            .add("currentCharge", currentCharge)
+            .add("capacity", capacity)
+            .build();
+        assertEquals(expected_capacity, actual_capacity, context, result ->
+            "Field capacity was not set to the expected value");
+        assertEquals(expected_currentCharge, actual_currentCharge, context, result ->
+            "Field currentCharge was not set to the expected value");
+        assertEquals("VESSEL", actual_transportType.toString(), context, result ->
+            "Field transportType was not set to the expected value");
+        //TODO: Nicht so clean
     }
 
 
@@ -189,13 +229,13 @@ public class TutorTests_H_3_2 {
         int[] currentCharge_TV = new int[]{10, 3, 33};
         int[] capacity_TV = new int[]{10, 1, -42};
 
-        for(int i = 0; i < currentCharge_TV.length; i++){
+        for (int i = 0; i < currentCharge_TV.length; i++) {
             test_constructor_single(currentCharge_TV[i], capacity_TV[i]);
         }
     }
 
     @Test
-    public void test_constructor_calls_setSpecificType(){
-        fail("Not implemented: Constructor calls setSpecificType");
+    public void test_constructor_calls_setSpecificType() {
+        fail(emptyContext(), result -> "Not implemented: Constructor calls setSpecificType");
     }
 }
--- src/graderPrivate/java/h05/TutorTests_H_3_3.java
+++ src/graderPrivate/java/h05/TutorTests_H_3_3.java
@@ -2,11 +2,13 @@ package h05;
 
 import org.junit.jupiter.api.Test;
 import org.sourcegrade.jagr.api.rubric.TestForSubmission;
+import org.tudalgo.algoutils.reflect.ClassTester;
+import org.tudalgo.algoutils.tutor.general.assertions.Context;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.tudalgo.algoutils.tutor.general.assertions.Assertions2.*;
 
 @TestForSubmission
 public class TutorTests_H_3_3 {
@@ -29,13 +31,13 @@ public class TutorTests_H_3_3 {
         var hybrid_type_1 = H05_Tester.HYBRID_TYPE_1_CT.get().resolve();
 
         var standardVoltageChargeable_field = hybrid_type_1.resolveAttribute(H05_Tester.HYBRID_TYPE_1_STANDARD_VOLTAGE_CHARGEABLE_AM.get());
-        standardVoltageChargeable_field.setAccessible(true);
+        standardVoltageChargeable_field.trySetAccessible();
         var highVoltageChargeable_field = hybrid_type_1.resolveAttribute(H05_Tester.HYBRID_TYPE_1_HIGH_VOLTAGE_CHARGEABLE_TYPE_AM.get());
-        highVoltageChargeable_field.setAccessible(true);
+        highVoltageChargeable_field.trySetAccessible();
         var fuelType_field = hybrid_type_1.resolveAttribute(H05_Tester.HYBRID_TYPE_1_FUEL_TYPE_AM.get());
-        fuelType_field.setAccessible(true);
+        fuelType_field.trySetAccessible();
         var averageConsumption_field = hybrid_type_1.resolveAttribute(H05_Tester.HYBRID_TYPE_1_AVERAGE_CONSUMPTION_AM.get());
-        averageConsumption_field.setAccessible(true);
+        averageConsumption_field.trySetAccessible();
 
 
         var instance = hybrid_type_1.resolveInstance();
@@ -51,32 +53,52 @@ public class TutorTests_H_3_3 {
         var actual_averageConsumption = H05_Tester.FUEL_DRIVEN_GET_AVERAGE_CONSUMPTION_MT.get().resolveMethod().invoke(instance, speed);
 
         // check Getter
-        assertEquals(expected_standardVoltageChargeable, actual_standardVoltageChargeable, "standardVoltageChargeable liefert nicht den korrekten Attributwert zurück.");
-        assertEquals(expected_highVoltageChargeable, actual_highVoltageChargeable, "highVoltageChargeable liefert nicht den korrekten Attributwert zurück.");
-        assertEquals(expected_fuelType, actual_fuelType, "getFuelType liefert nicht den korrekten Attributwert zurück.");
-        assertEquals(expected_averageConsumption, actual_averageConsumption, "getAverageConsumption liefert nicht den korrekten Attributwert zurück.");
-
+        Context context = contextBuilder()
+            .add("standardVoltageChargeable", expected_standardVoltageChargeable)
+            .add("highVoltageChargeable", expected_highVoltageChargeable)
+            .add("fuelType", expected_fuelType)
+            .add("averageConsumption", expected_averageConsumption)
+            .build();
+        assertEquals(expected_standardVoltageChargeable, actual_standardVoltageChargeable, context, result ->
+            "Method standardVoltageChargeable did not return the expected value");
+        assertEquals(expected_highVoltageChargeable, actual_highVoltageChargeable, context, result ->
+            "Method highVoltageChargeable did not return the expected value");
+        assertEquals(expected_fuelType, actual_fuelType, context, result ->
+            "Method getFuelType did not return the expected value");
+        assertEquals(expected_averageConsumption, actual_averageConsumption, context, result ->
+            "Method getAverageConsumption did not return the expected value");
 
         //check Toggle
         H05_Tester.HYBRID_TYPE_1_TOGGLE_STANDARD_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
-        assertNotEquals(expected_standardVoltageChargeable, hybrid_type_1.getFieldValue(standardVoltageChargeable_field));
+        assertNotEquals(expected_standardVoltageChargeable, hybrid_type_1.getFieldValue(standardVoltageChargeable_field), context, result ->
+            "Field standardVoltageChargeable did not have expected value after invoking toggleStandardVoltageChargeable once");
         H05_Tester.HYBRID_TYPE_1_TOGGLE_STANDARD_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
-        assertEquals(expected_standardVoltageChargeable, hybrid_type_1.getFieldValue(standardVoltageChargeable_field));
+        assertEquals(expected_standardVoltageChargeable, hybrid_type_1.getFieldValue(standardVoltageChargeable_field), context, result ->
+            "Field standardVoltageChargeable did not have expected value after invoking toggleStandardVoltageChargeable twice");
 
         H05_Tester.HYBRID_TYPE_1_TOGGLE_HIGH_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
-        assertNotEquals(expected_highVoltageChargeable, hybrid_type_1.getFieldValue(highVoltageChargeable_field));
+        assertNotEquals(expected_highVoltageChargeable, hybrid_type_1.getFieldValue(highVoltageChargeable_field), context, result ->
+            "Field highVoltageChargeable did not have expected value after invoking toggleHighVoltageChargeable once");
         H05_Tester.HYBRID_TYPE_1_TOGGLE_HIGH_VOLTAGE_CHARGEABLE_MT.get().resolveMethod().invoke(instance);
-        assertEquals(expected_highVoltageChargeable, hybrid_type_1.getFieldValue(highVoltageChargeable_field));
-
+        assertEquals(expected_highVoltageChargeable, hybrid_type_1.getFieldValue(highVoltageChargeable_field), context, result ->
+            "Field highVoltageChargeable did not have expected value after invoking toggleHighVoltageChargeable twice");
 
         //check Setter
-        Object new_averageConsumption = hybrid_type_1.getRandomValue(averageConsumption_field.getType());
+        Object new_averageConsumption = ClassTester.getRandomValue(averageConsumption_field.getType());
+        context = contextBuilder()
+            .add("averageConsumption (parameter)", new_averageConsumption)
+            .build();
         H05_Tester.HYBRID_TYPE_1_SET_AVERAGE_CONSUMPTION_MT.get().resolveMethod().invoke(instance, new_averageConsumption);
-        assertEquals(new_averageConsumption, hybrid_type_1.getFieldValue(averageConsumption_field), "setAverageConsumption funktioniert nicht korrekt.");
+        assertEquals(new_averageConsumption, hybrid_type_1.getFieldValue(averageConsumption_field), context, result ->
+            "setAverageConsumption did not set averageConsumption to the expected value");
 
-        Object new_fuelType = hybrid_type_1.getRandomValue(fuelType_field.getType());
+        Object new_fuelType = ClassTester.getRandomValue(fuelType_field.getType());
+        context = contextBuilder()
+            .add("fuelType (parameter)", new_fuelType)
+            .build();
         H05_Tester.HYBRID_TYPE_1_SET_FUEL_TYPE_MT.get().resolveMethod().invoke(instance, new_fuelType);
-        assertEquals(new_fuelType, hybrid_type_1.getFieldValue(fuelType_field), "setFuelType funktioniert nicht korrekt.");
+        assertEquals(new_fuelType, hybrid_type_1.getFieldValue(fuelType_field), context, result ->
+            "setFuelType did not set fuelType to the expected value");
     }
 
     @Test
@@ -86,20 +108,20 @@ public class TutorTests_H_3_3 {
 
     @Test
     public void test_HybridType2_hybrid_Object() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        var hybridObject_field =
-            H05_Tester.HYBRID_TYPE_2_CT.get().resolve().resolveAttribute(H05_Tester.HYBRID_TYPE_2_HYBRID_OBJECT_AM.get());
-        hybridObject_field.setAccessible(true);
+        var hybridObject_field = H05_Tester.HYBRID_TYPE_2_CT
+            .get().resolve().resolveAttribute(H05_Tester.HYBRID_TYPE_2_HYBRID_OBJECT_AM.get());
+        hybridObject_field.trySetAccessible();
 
-        var constructor =
-            H05_Tester.HYBRID_TYPE_2_CT.get().resolve().resolveConstructor(H05_Tester.HYBRID_TYPE_2_CONSTRUCTOR_PARAMETER_MATCHERS.get());
+        var constructor = H05_Tester.HYBRID_TYPE_2_CT
+            .get().resolve().resolveConstructor(H05_Tester.HYBRID_TYPE_2_CONSTRUCTOR_PARAMETER_MATCHERS.get());
         Object instance = constructor.newInstance();
         Object hybridObject = hybridObject_field.get(instance);
-        assertNotEquals(null, hybridObject, "hybridObject wird nicht im Konstruktor gesetzt.");
+
+        assertNotNull(hybridObject, emptyContext(), result -> "Field hybridObject was not set by the constructor");
     }
 
     @Test
     public void test_HybridType2_forwarding() {
-        fail("Not implemented: Hybrid2 calls Hybrid1");
+        fail(emptyContext(), result -> "Not implemented: Hybrid2 calls Hybrid1");
     }
-
 }
