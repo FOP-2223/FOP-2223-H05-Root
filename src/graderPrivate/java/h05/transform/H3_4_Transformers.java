921e237a35207bf2a65661c0bf0194b2fbc592db
Daniel Mangold <mangold.daniel@protonmail.com>
2022-12-16 23:14:43 +0100

    Fix allowed methods whitelist in H3.4

--- src/graderPrivate/java/h05/transform/H3_4_Transformers.java
+++ src/graderPrivate/java/h05/transform/H3_4_Transformers.java
@@ -1,5 +1,6 @@
 package h05.transform;
 
+import org.intellij.lang.annotations.Language;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
@@ -11,17 +12,18 @@ import static org.objectweb.asm.Opcodes.*;
 
 public class H3_4_Transformers {
 
-    private static final List<String> ALLOWED_METHODS = List.of(
-        "org/sourcegrade/jagr/core/executor/TimeoutHandler#checkTimeout()V",
-        "java/lang/String#<init>([C)V",
-        "java/lang/String#valueOf([C)Ljava/lang/String;",
-        "java/lang/String#charAt(I)C",
-        "java/lang/String#length()I",
-        "java/lang/String#toCharArray()[C",
-        "java/lang/Enum#toString()Ljava/lang/String;",
-        "java/lang/Enum#name()Ljava/lang/String;",
-        "java/lang/Object#toString()Ljava/lang/String;"
-    );
+    @Language("Regexp")
+    private static final String ALLOWED_METHODS = "(" +
+                                                  "org/sourcegrade/jagr/core/executor/TimeoutHandler#checkTimeout\\(\\)V|" +
+                                                  "java/lang/String#<init>\\(\\[C\\)V|" +
+                                                  "java/lang/String#charAt\\(I\\)C|" +
+                                                  "java/lang/String#length\\(\\)I|" +
+                                                  "java/lang/String#toCharArray\\(\\)\\[C|" +
+                                                  "java/lang/Enum#toString\\(\\)Ljava/lang/String;|" +
+                                                  "java/lang/Enum#name\\(\\)Ljava/lang/String;|" +
+                                                  "java/lang/Object#toString\\(\\)Ljava/lang/String;|" +
+                                                  "java/lang/\\w+#valueOf\\([^)]*\\).+" +
+                                                  ")";
     public static String illegalInstruction = null;
     public static final Function<ClassWriter, ClassVisitor> TRANSFORMER = writer -> new ClassVisitor(ASM9, writer) {
         @Override
@@ -31,7 +33,7 @@ public class H3_4_Transformers {
                     @Override
                     public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
                         String instruction = "%s#%s%s".formatted(owner, name, descriptor);
-                        if (!owner.startsWith("h05") && !ALLOWED_METHODS.contains(instruction)) {
+                        if (!owner.startsWith("h05") && !instruction.matches(ALLOWED_METHODS)) {
                             illegalInstruction = illegalInstruction == null ? instruction : illegalInstruction;
                         }
                         super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
